336c2d4891ea02e64f2442c281f4fa621bb524b3
Ian Clarkson <clarksonian@gmail.com>
2021-06-07 23:04:22 -0700

    Seems to be working

--- zoner.rb
+++ zoner.rb
@@ -1,7 +1,75 @@
+# https://en.wikipedia.org/wiki/M3U
+
 require 'faraday'
 
-recordingDurationSecs = ARGV[0].to_i
+<<-sh
+ruby zoner.rb 108000
+sh
 
-# Get a session ID
+recordingDurationSecs = ARGV[0].to_i
+targetDuration = nil
+
+def toFormat(time)
+  return time.utc.strftime('%Y%m%dT%H%M%SZ')
+end
+
+def getUrls(body)
+  return body.scan(/^(.*Z\.aac)$/).flatten
+end
+
+# Get a URL including session ID
 sessionIdResponse = Faraday.get('https://stream.jpbgdigital.com/CJZN/HEAAC/48k/playlist.m3u8')
-sessionId = sessionIdResponse.body.scan(/session_id=([\w-]+)/)
+sessionIdUrl = sessionIdResponse.body.lines.last
+puts sessionIdUrl
+
+# Determine recording duration
+startTime = Time.now
+endTime   = startTime + recordingDurationSecs
+puts "Recording from #{toFormat(startTime)} to #{toFormat(endTime)}"
+
+# Make a directory to store the files
+dirName = toFormat(startTime)
+Dir.mkdir(dirName)
+
+# Keep going until we've recorded enough
+while Time.now <= endTime
+  # Get the block info
+  blockResponse = Faraday.get(sessionIdUrl)
+
+  # Get target duration
+  targetDuration = blockResponse.body.scan(/TARGETDURATION:(\d+)/).flatten.first.to_i
+  puts "Target Duration: #{targetDuration}"
+
+  # Get the relevant URLs
+  urls = getUrls(blockResponse.body)
+  puts urls
+
+  # Walk through URLs and save ones we don't have
+  urls.each do |url|
+    basename = url.scan(/CJZN-\w+\.aac/)
+    puts basename
+
+    filename = File.join(dirName, basename)
+    if File.exist?(filename)
+      puts "Already have #{filename}, skipping"
+    else
+      puts "Getting #{filename}"
+
+      File.write(filename, Faraday.get(url).body)
+    end
+  end
+
+  # Wait the target duration to check for a new file
+  sleep targetDuration
+end
+
+puts "Done recording!"
+
+# Assemble everything into a single file
+filenames = Dir.glob(File.join(dirName, '*.aac'))
+outputFile = File.join(dirName, 'output.aac')
+cmd = %{ffmpeg -i "concat:#{filenames.join('|')}" -c copy #{outputFile}}
+system(cmd)
+
+# Delete the parts that made up the whole
+filenames.each { |f| File.delete(f) }
