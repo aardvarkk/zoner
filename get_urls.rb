f8761ee58fb64506fe05fcd0ff77c700484ab366
Ian Clarkson <clarksonian@gmail.com>
2021-06-11 14:14:26 -0700

    Add support for ignoring "Ad Butler"
    - Only look for files containing CJZN in them
    - Add back HTTP scheme since we can't pull it out of the parsing as it's URL-encoded in the doubleclick tracking

--- get_urls.rb
+++ get_urls.rb
@@ -1,3 +1,3 @@
 def get_urls(body)
-  return body.scan(/\/\/([\w\/.-]+\.aac)/).flatten
+  return body.scan(/\/\/([\w\/.]+CJZN-[\w-]+\.aac)/).flatten.map { "http://#{_1}" }
 end
--- test.rb
+++ test.rb
@@ -3,7 +3,11 @@ require './get_urls'
 TEST_FAILURE = "Test failure!"
 
 urls = get_urls(File.read('test_files/playlist.m3u8'))
-puts urls
+# puts urls
 raise TEST_FAILURE unless urls.size == 8
+
+urls = get_urls(File.read('test_files/ad_butler.m3u8'))
+# puts urls
+raise TEST_FAILURE unless urls.size == 3
 
 puts "Test success!"
